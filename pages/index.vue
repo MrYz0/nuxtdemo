b0df96708eb8e110728a90a6b88123205ed38683
‘MrYz0’ <2569901191@qq.com>
2022-08-04 18:14:18 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+ <div>
+   <NuxtLink to="/teacher">教师</NuxtLink>
+   <h1>主页面</h1>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/teacher/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    教师页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {}
+  },
+  created() {
+
+  },
+  methods: {},
+
+}
+</script>
+
+<style scoped>
+
+</style>
